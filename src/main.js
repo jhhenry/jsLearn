d0f31ea4efb401ea0845f1bc8bc2bc69a7ee2ab3
Hao Jin <jh@highsharp.com>
2018-09-18 10:53:54 +0800

    draw  cirle using paperjs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
     'use strict';
-    console.log('main.js loaded');
+    // console.log('main.js loaded');
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+    var c = Shape.Circle(200, 200, 50);
+    c.fillColor = 'green';
+    paper.view.draw();
 })
