01ce84e256849e1fbd5eabaca9577214478cd640
Hao Jin <jh@highsharp.com>
2018-09-18 11:31:00 +0800

    use PointText

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ $(document).ready(function () {
     // console.log('main.js loaded');
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
-    
-    let tool = new Tool();
-    tool.onMouseDown = function(event) {
-        let c = Shape.Circle(event.point.x, event.point.y, 20)
-        c.fillColor = 'green'
-    }
+
+    // let tool = new Tool();
+    // tool.onMouseDown = function(event) {
+    //     let c = Shape.Circle(event.point.x, event.point.y, 20)
+    //     c.fillColor = 'green'
+    // }
+
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello world';
 })
