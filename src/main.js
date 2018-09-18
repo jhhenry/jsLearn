c66683563931bd7dba06a0d943ceef3dc3e8e4c1
Hao Jin <jh@highsharp.com>
2018-09-18 11:10:14 +0800

    use Tool and mouseDown event to draw cirles in the canvas

--- src/main.js
+++ src/main.js
@@ -3,11 +3,10 @@ $(document).ready(function () {
     // console.log('main.js loaded');
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
-    var c;
-    for (let x = 25; x < 400; x += 50) {
-        for (let y = 25; y < 400; y += 50) {
-            c = Shape.Circle(x, y, 20);
-            c.fillColor = 'green';
-        }
+    
+    let tool = new Tool();
+    tool.onMouseDown = function(event) {
+        let c = Shape.Circle(event.point.x, event.point.y, 20)
+        c.fillColor = 'green'
     }
 })
