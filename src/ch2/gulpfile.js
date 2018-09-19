3ac7debbde20d11418cfd317bad28d1522de5b14
Hao Jin <jh@highsharp.com>
2018-09-19 15:33:30 +0800

    use eslint

--- src/ch2/gulpfile.js
+++ src/ch2/gulpfile.js
@@ -1,16 +1,24 @@
-const gulp = require('gulp')
+const gulp = require("gulp");
 const sourcemaps = require("gulp-sourcemaps");
 const babel = require("gulp-babel");
+const eslint = require("gulp-eslint");
 //const concat = require("gulp-concat");
 
-gulp.task('default', function () {
-    gulp.src("es6/**/*.js")
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        //.pipe(concat('all.js'))
-        .pipe(sourcemaps.write('../maps'))
-        .pipe(gulp.dest('dist'))
-    gulp.src("public/es6/**/*.js")
-        .pipe(babel())
-        .pipe(gulp.dest("public/dist"))
-})
+gulp.task("default", function () {
+
+	gulp.src("es6/**/*.js")
+		.pipe(sourcemaps.init())
+		.pipe(babel())
+	//.pipe(concat('all.js'))
+		.pipe(sourcemaps.write("../maps"))
+		.pipe(gulp.dest("dist"));
+	gulp.src("public/es6/**/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("public/dist"));
+});
+
+gulp.task("eslint", function () {
+	gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+		.pipe(eslint())
+		.pipe(eslint.format());
+});
--- src/ch2/public/es6/test.js
+++ src/ch2/public/es6/test.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 const sentences = [{ subject: 'JavaScript', verb: 'is', object: 'great' },
-{ subject: 'Elephants', verb: 'are', object: 'large' },];
+	{subject: 'Elephants', verb: 'are', object: 'large' },];
 // es6 feature: object destructuring
 function say({ subject, verb, object }) {
-    // es6 feature: template strings
-    console.log(`${subject} ${verb} ${object}`);
+	// es6 feature: template strings
+	console.log(`${subject} ${verb} ${object}`);
 }
 // es6 feature: for..of
 for (let s of sentences) {
-    say(s);
+	say(s);
 }
