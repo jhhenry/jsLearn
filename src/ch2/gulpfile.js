a7fcbbc4b7d4f6b830efa79298c7ff1880235abf
Hao Jin <jh@highsharp.com>
2018-09-19 15:01:35 +0800

    use gulp + babel

---
+++ src/ch2/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+const sourcemaps = require("gulp-sourcemaps");
+const babel = require("gulp-babel");
+//const concat = require("gulp-concat");
+
+gulp.task('default', function () {
+    gulp.src("es6/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        //.pipe(concat('all.js'))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('dist'))
+    gulp.src("public/es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist"))
+})
