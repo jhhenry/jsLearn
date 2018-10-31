8d80bed765c8ccc6106c6074f3d4ce40f72a9b47
Hao Jin <jh@highsharp.com>
2018-10-31 11:35:00 +0800

    use ava and convert-hex

--- src/ava/test.js
+++ src/ava/test.js
@@ -1,4 +1,5 @@
 import test from 'ava'
+const convertHex = require('convert-hex')
 //import utils from './utils'
 test('foo', t => {
 	// for (let p in utils) {console.log(`${p}: ${utils[p]}`)}
@@ -9,4 +10,10 @@ test('foo', t => {
 test('bar', async t=> {
 	const bar = Promise.resolve('bar');
 	t.is(await bar, 'bar');
+});
+
+test('try convert-hex', t=> {
+	var hex = "0x34550122DF" //"0x" prefix is optional
+	//console.dir(convertHex.hexToBytes(hex).join(',')) //'[52,85,1,34,223]'
+	t.deepEqual(convertHex.hexToBytes(hex), [52,85,1,34,223]);
 });
