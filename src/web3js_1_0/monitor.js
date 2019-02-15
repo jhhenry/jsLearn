605e37f30a730ae1234f3ea43da807935752d7fc
Hao Jin <jh@highsharp.com>
2019-02-15 17:37:51 +0800

    use web3 1.0 to new contract and listen to events

--- src/web3js_1_0/monitor.js
+++ src/web3js_1_0/monitor.js
@@ -1,24 +1,91 @@
 const Web3 = require('web3');
 
-const web3 = new Web3("http://192.168.56.123:8565");
+const provider = new Web3.providers.WebsocketProvider("ws://192.168.56.123:8566", {headers: {
+    Origin: "http://localhost"
+}});
+provider.on('error', e => console.error('WS Error', e));
+provider.on('end', e => console.error('WS End', e));
+
+const web3 = new Web3(provider);
 const net = require('net');
+
 //console.log(web3);
 
 //web3.personal.unLockAccount
+//console.log(web3.eth.accounts);
+readContractAddress().then((r, err) => {
+    if (err) {
+        console.log("Got an error", err);
+        return;
+    }
+    const addr = r;
+   // console.log("Finally got the addr", addr);
+    // construct the contract instance with the address
+    try {
+        const lotteryC = new web3.eth.Contract(addr.abi, addr.address);
+        console.log("start monitoring RedeemedLottery events");
+        lotteryC.events.RedeemedLotttery().on(
+            "data", e => {
+                console.log(e.returnValues);
+        })
+    } catch(e) {
+        console.error(e);
+    }
+   
+   // console.log("lottery contract", lotteryC.methods.getPledge);
+    
+    //web3.eth.getAccounts().then(console.log)
+    //.then(arr => {
+        // console.log("getAccounts result:", arr);
+        // arr.forEach(e => {
+        //     lotteryC.methods.getPledge.call({from:e}).then(console.log);
+        // })
+        
+    // })
+    // start monitoring the specific event of the contract
+    // subscribe the event
+   
+});
 
-readContractAddress();
 function readContractAddress()
 {
+    let contractInfo;
     const client = net.createConnection({  host: 'localhost', port: 8124 }, () => {
         // 'connect' listener
         console.log('connected to server!');
-        client.write('world!\r\n');
+        client.write('request contract address');
+        //client.pipe(client);
     });
     client.on('data', (data) => {
-        console.log("received data:", data.toString());
-        client.end();
+        const data_str = data.toString();
+        console.log("received data:", data_str.substr(0, 200));
+        if (data_str.startsWith('{"address":')) {
+            contractInfo = JSON.parse(data_str);
+            console.log("got addr 1:", contractInfo.address);
+            client.end();
+        }
+        
     });
     client.on('end', () => {
         console.log('disconnected from server');
+    });
+    return new Promise((resolve, reject) => {
+        console.log("Enter promise logic");
+        let tryTime = 0;
+        const checkFunc = function() {
+            if (contractInfo) {
+                //console.log("got addr 2:", contractInfo);
+                resolve(contractInfo);
+            } else {
+                tryTime++;
+                if(tryTime >= 5) {
+                    reject("timeout");
+                }
+                setTimeout(checkFunc, 1000);
+            }
+            
+        };
+        checkFunc();
+
     });
 }
