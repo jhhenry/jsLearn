3ec4e267e9287686357c11ed2c06f4268c440770
Hao Jin <jh@highsharp.com>
2019-03-22 16:45:36 +0800

    more tests for regEx's group

--- src/nodejs_tutorial/regEx.js
+++ src/nodejs_tutorial/regEx.js
@@ -1,9 +1,18 @@
-function testGroup()
+function testGroup(str)
 {
-    const str = 'root@locahost:9090/test';
     const regEx = /([^@]+)@([^:]+):(\d+)\/(.*)/;
     const rs = str.match(regEx);
+   
     console.log(rs);
+    if (rs && rs.length >= 5) {
+        console.log( );
+        return rs.slice(1,5);
+    }
+    return [];
 }
 
-testGroup();
+let [user, host, port, database] = testGroup('root@locahost:9090/test');
+console.log(`${user}@${host}:${port}/${database}`);
+
+[user, host, port, database] = testGroup('root@locahost:9090');
+console.log(`${user}@${host}:${port}/${database}`);
